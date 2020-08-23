5c4c330781032a0a4d683c33c88bcddc121d2f4e
Rishabh Anand <rishabhanand42@gmail.com>
2020-08-24 01:22:57 +0530

    simple time picker and date picker testing  On branch master  Changes to be committed: 	modified:   ios/Podfile.lock 	modified:   package.json 	modified:   screens/Chart/index.js 	modified:   yarn.lock

--- screens/Chart/index.js
+++ screens/Chart/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  ScrollView,
+  Text,
+  Button,
+  Platform,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+const {width, height} = Dimensions.get('window');
 
 export default function (props) {
+  const [show, setShow] = useState(false);
+  const [mode, setMode] = useState('date');
+  const [date, setDate] = useState(new Date());
   return (
-    <View>
-      <Text>Chart</Text>
-    </View>
+    <ScrollView style={styles.view}>
+      <Text>Birth Date</Text>
+      <Button
+        onPress={() => {
+          setShow(true);
+          setMode('date');
+        }}
+        title="Open Date Picker"
+      />
+      <Text>Birth Time</Text>
+      <Button
+        onPress={() => {
+          setShow(true);
+          setMode('time');
+        }}
+        title="Open Time Picker"
+      />
+      {show && (
+        <DateTimePicker
+          mode={mode}
+          value={date}
+          is24Hour={true}
+          display="spinner"
+          onChange={(ev, val) => {
+            setDate(val);
+            setShow(false);
+          }}
+        />
+      )}
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    ...StyleSheet.absoluteFillObject,
+    minHeight: height,
+  },
+});
