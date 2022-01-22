87b2ab092bb0cc53116546e14903e7ae6c441a64
Rishabh Anand <rishab@caarya.in>
2022-01-22 16:30:16 +0530

    upgrade storage to be persistent forever

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,11 @@
+import {AsyncStorage} from '@react-native-community/async-storage';
+
 const DataStore = require('react-native-local-mongodb');
 const db = new DataStore({
   timestampData: true,
+  autoload: true,
+  storage: AsyncStorage,
+  filename: 'charts_data',
 });
 
 export default db;
