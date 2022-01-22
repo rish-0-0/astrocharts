0471a45002be50eaa1506d3d60c8f0397f2a62a6
Rishabh Anand <rishab@caarya.in>
2022-01-22 16:52:35 +0530

    trying the deprecated API

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,11 +1,11 @@
-import {AsyncStorage} from '@react-native-community/async-storage';
+import {AsyncStorage} from 'react-native';
 
 const DataStore = require('react-native-local-mongodb');
 const db = new DataStore({
   timestampData: true,
-  autoload: true,
   storage: AsyncStorage,
-  filename: 'charts_data',
+  filename: 'asyncStorageKey',
+  autoload: true,
 });
 
 export default db;
